dd9f68c13b7c0f509260b9151522bb2aa5b052b0
CamilaL <plopezmurgueitio@gmail.com>
2025-05-16 10:30:57 -0500

    Add Login

---
+++ app/src/main/java/com/lopez/paula/cazarpatos/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.lopez.paula.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var ButtonLogin : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ButtonLogin.findViewById<Button>(R.id.ButtonLogin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        ButtonLogin.setOnClickListener {
+//           val intent = Intent (this, MainActivity::class.java)
+//            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
