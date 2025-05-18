a909bfee5a278a5024856107a9603bb466b4847c
CamilaL <plopezmurgueitio@gmail.com>
2025-05-17 22:28:02 -0500

    arrangements

--- app/src/main/java/com/lopez/paula/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/lopez/paula/cazarpatos/LoginActivity.kt
@@ -2,10 +2,7 @@ package com.lopez.paula.cazarpatos
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,7 +14,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ButtonLogin.findViewById<Button>(R.id.ButtonLogin)
+        ButtonLogin=findViewById<Button>(R.id.ButtonLogin)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
